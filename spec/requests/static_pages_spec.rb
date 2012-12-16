3ff3d76124ff3c5461b299ea232909fe7cb744aa
Franco Mercalli <franco@studiomercalli.it>
2012-12-16 22:09:04 +0100

    finish feed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,6 +31,17 @@ describe "StaticPages" do
         end
       end
       
+      describe 'follower/followers counts' do
+        let(:other_user) {FactoryGirl.create(:user)}
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+        
+        it {should have_link('0 following', href: following_user_path(user))}
+        it {should have_link('1 follower', href: followers_user_path(user))}
+      end
+      
       it 'should have micropost count' do
         should  have_content('2 microposts')
       end
